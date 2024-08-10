e04c04de47f4aa229b9ffd0014b9e5adc2ae0fb5
yojun313 <moonyojun@gmail.com>
2024-08-10 12:30:03 +0900

    1.0.4 beta

--- MANAGER/test.py
+++ MANAGER/test.py
@@ -3,6 +3,12 @@ import os
 import subprocess
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 
+# KIMKEM.py의 내용
+KIMKEM_CODE = """
+# KIMKEM.py의 내용
+print("KIMKEM.py is running")
+"""
+
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -21,14 +27,16 @@ class MyApp(QWidget):
         self.setLayout(layout)
 
     def run_kimkem(self):
-        # 현재 디렉토리의 경로를 가져옵니다.
-        script_path = os.path.join(os.getcwd(), 'KIMKEM.py')
+        # KIMKEM.py 파일을 임시 디렉토리에 생성합니다.
+        temp_dir = os.path.dirname(os.path.abspath(sys.executable))
+        script_path = os.path.join(temp_dir, 'KIMKEM.py')
+
+        with open(script_path, 'w') as file:
+            file.write(KIMKEM_CODE)
 
         if sys.platform == "win32":
-            # Windows에서 새로운 cmd 창에서 실행
             subprocess.Popen(['start', 'cmd', '/k', f'python {script_path}'], shell=True)
         elif sys.platform == "darwin":
-            # MacOS에서 새로운 터미널에서 실행
             subprocess.Popen(['osascript', '-e', f'tell application "Terminal" to do script "python3 {script_path}"'])
         else:
             print("지원되지 않는 운영체제입니다.")
