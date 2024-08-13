df62d594ee9fc8cef2d97a427b31def43399dca5
yojun313 <moonyojun@gmail.com>
2024-08-13 09:41:45 +0900

    Update BIGMACLAB_MANAGER.py

--- MANAGER/BIGMACLAB_MANAGER.py
+++ MANAGER/BIGMACLAB_MANAGER.py
@@ -22,7 +22,7 @@ import socket
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        self.versionNum = '1.1.0'
+        self.versionNum = '1.1.1'
         self.version = 'Version ' + self.versionNum
 
         super(MainWindow, self).__init__()
