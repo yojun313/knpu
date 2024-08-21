cf6b9910654e498cede18effe74e46bd474bd48b
yojun313 <moonyojun@gmail.com>
2024-08-22 01:16:15 +0900

    proxy list update

--- CRAWLER/IP_UPDATE.py
+++ CRAWLER/IP_UPDATE.py
@@ -1,2 +1,21 @@
 from Package.ToolModule import ToolModule
+import os
 
+ToolModule_obj = ToolModule()
+
+pathfinder_obj = ToolModule_obj.pathFinder()
+
+mysql_obj = pathfinder_obj['MYSQL']
+proxy_path = os.path.join(pathfinder_obj['crawler_folder_path'], '아이피샵(유동프록시).txt')
+
+proxy_list = ToolModule_obj.read_txt(proxy_path)
+proxy_list = [[proxy] for proxy in proxy_list]
+
+mysql_obj.connectDB('crawler_db')
+try:
+    mysql_obj.droptable('proxy_list')
+except:
+    pass
+
+mysql_obj.newTable('proxy_list', ['proxy'])
+mysql_obj.insertToTable('proxy_list', proxy_list)
