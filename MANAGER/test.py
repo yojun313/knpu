75e7eaccf736b3d67f1bab301664a3db4a1cb567
yojun313 <moonyojun@gmail.com>
2024-08-27 10:20:24 +0900

    Update test.py

--- MANAGER/test.py
+++ MANAGER/test.py
@@ -1,25 +1,13 @@
-def remove_trailing_empty_values(dictionary):
-    # 딕셔너리의 키 리스트를 역순으로 가져옴
-    keys = list(dictionary.keys())
-
-    # 역순으로 빈 리스트를 가진 key를 제거
-    for key in reversed(keys):
-        if dictionary[key] == []:
-            del dictionary[key]
-        else:
-            break  # 빈 리스트가 아닌 값을 만나면 멈춤
-
-    return dictionary
-
-# 예시 딕셔너리
-my_dict = {
-    'a': [1, 2],
-    'b': [],
-    'c': [],
-    'd': [3],
-    'e': []
-}
-
-# 함수 호출
-updated_dict = remove_trailing_empty_values(my_dict)
-print(updated_dict)
+def gpt_generate(self, query):
+    self.api_key = "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"
+    client = OpenAI(api_key=self.api_key)
+    model = "gpt-4"
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": query},
+        ]
+    )
+    content = response.choices[0].message.content
+    return content
