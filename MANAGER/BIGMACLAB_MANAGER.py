166b82c15745cb3d61c9402c617602f9a44bdc01
yojun313 <moonyojun@gmail.com>
2024-08-29 18:43:59 +0900

    update 1.5.3

--- MANAGER/BIGMACLAB_MANAGER.py
+++ MANAGER/BIGMACLAB_MANAGER.py
@@ -28,7 +28,7 @@ warnings.filterwarnings("ignore")
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        self.versionNum = '1.5.2'
+        self.versionNum = '1.5.3'
         self.version = 'Version ' + self.versionNum
          
         super(MainWindow, self).__init__()
