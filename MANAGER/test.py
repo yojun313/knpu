0b9f090af78d9f3b42b06b31206d0f6008435c7a
yojun313 <moonyojun@gmail.com>
2024-09-11 11:55:29 +0900

    update

--- MANAGER/test.py
+++ MANAGER/test.py
@@ -1,27 +1,15 @@
-import pandas as pd
+import os
+import requests
 
-# 예시 데이터프레임
-data = {'Article Text': [
-    '사과와 바나나는 맛있다',
-    '나는 당근과 오이를 좋아해',
-    '포도와 바나나는 건강에 좋다',
-    '사과는 신선하고 당근은 건강하다',
-    '오렌지와 배는 맛있다'
-]}
+def download_new_version(download_url, local_filename):
+    response = requests.get(download_url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
 
-tableDF = pd.DataFrame(data)
+    print(f"{local_filename} 다운로드 완료")
 
-# 포함 단어 리스트
-incl_words = ['사과', '바나나']
-
-# 제외 단어 리스트
-excl_words = ['당근', '오이']
-
-# incl_words에 있는 단어 중 하나라도 포함된 행을 필터링
-tableDF = tableDF[tableDF['Article Text'].apply(lambda cell: any(word in cell for word in incl_words))]
-
-# excl_words에 있는 단어가 하나도 포함되지 않은 행을 필터링
-tableDF = tableDF[tableDF['Article Text'].apply(lambda cell: all(word not in cell for word in excl_words))]
-
-# 결과 출력
-print(tableDF)
+# 최신 버전의 exe 파일 다운로드
+download_url = "https://knpu.re.kr:90/download/BIGMACLAB_MANAGER_1.7.0.exe"
+local_path = "/Users/yojunsmacbookprp/Desktop/BIGMACLAB_MANAGER/BIGMACLAB_MANAGER_1.7.0.exe"
+download_new_version(download_url, local_path)
