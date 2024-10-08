7dbcebe9d8ceaeaeba952b528f91871f23ea70b7
yojun313 <moonyojun@gmail.com>
2024-10-08 10:26:41 +0900

    Update BIGMACLAB_MANAGER.py

--- MANAGER/BIGMACLAB_MANAGER.py
+++ MANAGER/BIGMACLAB_MANAGER.py
@@ -28,7 +28,7 @@ warnings.filterwarnings("ignore")
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        self.versionNum = '1.8.1'
+        self.versionNum = '1.8.2'
         self.version = 'Version ' + self.versionNum
          
         super(MainWindow, self).__init__()
