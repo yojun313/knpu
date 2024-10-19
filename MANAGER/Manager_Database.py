d6383eac68b84754c44a6621afabc2d192048a19
yojun313 <moonyojun@gmail.com>
2024-10-19 11:15:45 +0900

    Update Manager_Database.py

--- MANAGER/Manager_Database.py
+++ MANAGER/Manager_Database.py
@@ -190,6 +190,7 @@ class Manager_Database:
                         self.DBtable_window = TableWindow(self.main, target_DB)
                         self.DBtable_window.destroyed.connect(destory_table)
                         self.DBtable_window.show()
+                    self.close_console()
 
                 QTimer.singleShot(1, load_database)
                 QTimer.singleShot(1, self.main.printStatus)
