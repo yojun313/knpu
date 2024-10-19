879ad18972a4edfc29ca0bb72898da188dca3f56
yojun313 <moonyojun@gmail.com>
2024-10-19 11:00:38 +0900

    update

--- MANAGER/Manager_Console.py
+++ MANAGER/Manager_Console.py
@@ -4,15 +4,17 @@ class Console():
     def __init__(self):
         self.console_process = None
 
-
     def open_console(self):
         """콘솔 창을 열고 프로세스를 유지"""
         if self.console_process is None:  # 콘솔이 열려있지 않다면
+            # /K 옵션을 사용해서 명령어가 실행된 후에도 콘솔 창을 유지
             self.console_process = subprocess.Popen(
-                'cmd.exe /k',  # 명령을 실행한 후에도 종료하지 않음
+                ['cmd.exe', '/K'],  # /K를 통해 콘솔이 종료되지 않도록 유지
                 stdin=subprocess.PIPE,
-                creationflags=subprocess.CREATE_NEW_CONSOLE,
-                text=True  # 텍스트 모드 사용
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                creationflags=subprocess.CREATE_NEW_CONSOLE
             )
 
     def run_function_in_console(self, message):
@@ -21,6 +23,5 @@ class Console():
 
         # 콘솔에 메시지 출력 명령 전달
         if self.console_process is not None:
-            self.console_process.stdin.write(f'echo {message} & pause\n')  # 메시지를 출력하고 대기
+            self.console_process.stdin.write(f'echo {message}\n')
             self.console_process.stdin.flush()  # 파이프에 강제로 보내기
-
