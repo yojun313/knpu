bd6c693bbad6a0dc4c59c88b801e91e30b070d83
yojun313 <moonyojun@gmail.com>
2024-11-02 15:52:54 +0900

    Update IP_UPDATE.py

--- CRAWLER/IP_UPDATE.py
+++ CRAWLER/IP_UPDATE.py
@@ -12,7 +12,7 @@ proxy_list = ToolModule_obj.read_txt(proxy_path)
 proxy_list = [[proxy] for proxy in proxy_list]
 
 mysql_obj.connectDB('crawler_db')
-mysql_obj.droptable('proxy_list')
+mysql_obj.dropTable('proxy_list')
 mysql_obj.commit()
 mysql_obj.newTable('proxy_list', ['proxy'])
 mysql_obj.insertToTable('proxy_list', proxy_list)
