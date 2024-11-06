0180026c06e6061fac1e6cdc5a31d8863ee269f4
yojun313 <moonyojun@gmail.com>
2024-11-06 10:36:11 +0900

    Update BIGMACLAB_MANAGER.py

--- MANAGER/BIGMACLAB_MANAGER.py
+++ MANAGER/BIGMACLAB_MANAGER.py
@@ -360,6 +360,7 @@ class MainWindow(QtWidgets.QMainWindow):
             current_version = version.parse(self.versionNum)
             self.new_version = version.parse(self.Manager_Board_obj.board_version_newcheck())
             if current_version < self.new_version:
+                self.Manager_Board_obj.board_version_refresh()
                 version_info_html = f"""
                     <style>
                         table {{
