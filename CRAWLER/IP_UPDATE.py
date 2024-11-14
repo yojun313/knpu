f33e347553f610b03ebb4529fe0e1a8174e074bf
yojun313 <moonyojun@gmail.com>
2024-11-14 21:41:04 +0900

    Update IP_UPDATE.py

--- CRAWLER/IP_UPDATE.py
+++ CRAWLER/IP_UPDATE.py
@@ -11,23 +11,19 @@ sys.path.append(MANAGER_PATH)
 from mySQL import mySQL
 
 def read_txt(filepath):
-    txt_path = filepath
     result_list = []
 
-    # 파일을 바이너리 모드로 열어 raw 데이터 읽기
-    with open(txt_path, 'rb') as file:
-        raw_data = file.read()
+    # 파일의 일부만 읽어 인코딩 감지
+    with open(filepath, 'rb') as file:
+        raw_data = file.read(10240)  # 처음 10KB만 읽음
         result = chardet.detect(raw_data)
         charenc = result['encoding']
 
     # 감지된 인코딩을 사용하여 파일을 텍스트 모드로 읽기
-    with open(txt_path, 'r', encoding=charenc) as f:
+    with open(filepath, 'r', encoding=charenc) as f:
         lines = f.readlines()
 
-    for element in lines:
-        element = element.replace('\n', '')
-        result_list.append(element)
-
+    result_list = [line.strip() for line in lines]
     return result_list
 
 mysql_obj = mySQL(host='121.152.225.232', user='admin', password='bigmaclab2022!', port=3306)
@@ -36,7 +32,7 @@ proxy_path = os.path.join("D:/BIGMACLAB/CRAWLER", '아이피샵(유동프록시)
 print("MYSQL 접속 완료\n")
 
 proxy_list = read_txt(proxy_path)
-proxy_list = [[proxy] for proxy in proxy_list]
+proxy_list = [[proxy.strip()] for proxy in proxy_list]
 
 print("IP 리스트 초기화 중... ", end='')
 mysql_obj.connectDB('crawler_db')
