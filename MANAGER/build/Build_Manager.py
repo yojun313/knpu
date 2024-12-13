49ed13d432e79a36c8d4f754b5285d221605e88e
yojun313 <moonyojun@gmail.com>
2024-12-13 14:00:09 +0900

    update

--- MANAGER/build/Build_Manager.py
+++ MANAGER/build/Build_Manager.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         output_directory = "D:/BIGMACLAB/BIGMACLAB_MANAGER/exe"
 
     # Spec file path
-    spec_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'BIGMACLAB_MANAGER.spec')
+    spec_file = os.path.join(os.path.dirname(__file__), 'BIGMACLAB_MANAGER.spec')
 
     while True:
         # Get the version from the user
