a2cb4e2de0752bb6f614a5c5079b69a0a1751132
CSMAC <moonyojun@gmail.com>
2025-01-30 01:42:58 +0900

    Update Manager_Database.py

--- MANAGER/Manager_Database.py
+++ MANAGER/Manager_Database.py
@@ -691,9 +691,11 @@ class Manager_Database:
             self.main.program_bug_log(traceback.format_exc())
 
     def database_search_llm_toggle(self):
+        #open_console("MANAGER LLM")
         print("hello")
         script_path = os.path.join(os.path.dirname(__file__), 'source', "LLM_Chat.exe")
-        subprocess.Popen([script_path], shell=True)
+        subprocess.Popen([script_path])
+
         return
 
     def database_search_llm(self, speech=False):
