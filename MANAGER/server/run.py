babf72b25741da63f04755c95626796ba32b484f
yojun313 <moonyojun@gmail.com>
2025-05-04 23:21:55 +0900

    update

--- MANAGER/server/run.py
+++ MANAGER/server/run.py
@@ -2,4 +2,11 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "run:app",               # or your app path
+        host="0.0.0.0",
+        port=8000,
+        timeout_keep_alive=86400,
+        http_timeout=86400,
+        reload=True              # 개발환경이라면
+    )
