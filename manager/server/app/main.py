5b47d4cc7a95a783653a1f2102394c394436cd50
yojun313 <moonyojun@gmail.com>
2025-05-17 03:38:05 +0900

    update

--- manager/server/app/main.py
+++ manager/server/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from app.routes import api_router
+import gc
+import asyncio
+
+async def periodic_gc(interval_seconds: int = 60):
+    while True:
+        await asyncio.sleep(interval_seconds)
+        gc.collect()
 
 app = FastAPI()
+@app.on_event("startup")
+async def start_background_gc():
+    asyncio.create_task(periodic_gc(60))
 
 app.include_router(api_router, prefix="/api", tags=["API"])
