410f9ab4debfeaaa31ee8a5727a07344dd551d72
yojun313 <moonyojun@gmail.com>
2025-06-01 10:57:42 +0900

    update

--- manager/server/app/utils/getsize.py
+++ manager/server/app/utils/getsize.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 
 def getFolderSize(path):
-    total_bytes = sum(f.stat().st_size for f in Path(
-        path).rglob('*') if f.is_file())
+    total_bytes = sum(f.stat().st_size for f in Path(path).rglob('*') if f.is_file())
     size_gb = round(total_bytes / (1024 ** 3), 1)
-    size_mb_int = round(total_bytes / (1024 ** 2), 1)
+    size_mb_int = int(total_bytes / (1024 ** 2))
     return [size_gb, size_mb_int]
