adbb5c1f7457e4c0cc56870517363ff6e95efb62
yojun313 <moonyojun@gmail.com>
2025-06-23 20:35:26 +0900

    update

--- manager/server/app/routes/__init__.py
+++ manager/server/app/routes/__init__.py
@@ -5,6 +5,7 @@ from .board_routes import router as board_router
 from .auth_routes import router as auth_router
 from .analysis_routes import router as analysis_router
 from .ping_routes import router as ping_router
+from .format_routes import router as format_router
 from fastapi import Depends
 from app.libs.jwt import verify_token
 
@@ -14,5 +15,6 @@ api_router.include_router(user_router, prefix="/users", tags=["Users"], dependen
 api_router.include_router(crawl_router, prefix="/crawls", tags=["Crawls"], dependencies=[Depends(verify_token)])
 api_router.include_router(board_router, prefix="/board", tags=["Board"], dependencies=[Depends(verify_token)])
 api_router.include_router(analysis_router, prefix="/analysis", tags=["Analysis"], dependencies=[Depends(verify_token)])
+api_router.include_router(format_router, prefix="/format", tags=["Format"], dependencies=[Depends(verify_token)])
 api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 api_router.include_router(ping_router, prefix="/ping", tags=["Ping"])
