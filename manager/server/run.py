c013c1bfb22a2ed6d44d9c3f1527662f3101c92d
yojun313 <moonyojun@gmail.com>
2025-10-25 02:52:37 +0900

    update

--- manager/server/run.py
+++ manager/server/run.py
@@ -1,8 +1,7 @@
 # gunicorn app.main:app -c run.py
-import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1       
+workers = 5
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 0
 loglevel = "warning"
