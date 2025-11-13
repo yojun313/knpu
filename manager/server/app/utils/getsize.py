56663d4d074fddbc55bfbef4b250f52773eecad1
yojun313 <moonyojun@gmail.com>
2025-11-14 01:23:26 +0900

    update

--- manager/server/app/utils/getsize.py
+++ manager/server/app/utils/getsize.py
@@ -11,11 +11,11 @@ def format_size(bytes_size):
 
     kb = bytes_size / 1024
     if kb < 1024:  # 1MB 미만
-        return f"{kb:.2f} KB"
+        return f"{kb:.0f} KB"
 
     mb = kb / 1024
     if mb < 1024:  # 1GB 미만
-        return f"{mb:.2f} MB"
+        return f"{mb:.0f} MB"
 
     gb = mb / 1024
-    return f"{gb:.3f} GB"
+    return f"{gb:.1f} GB"
