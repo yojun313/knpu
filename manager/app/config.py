349fcded57c22a59ef7bb455d528380fe5c2b09c
yojun313 <moonyojun@gmail.com>
2025-11-22 12:53:52 +0900

    Update mode in config.py to enable production environment

--- manager/app/config.py
+++ manager/app/config.py
@@ -10,7 +10,7 @@ ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
 ADMIN_PUSHOVERKEY = 'uvz7oczixno7daxvgxmq65g2gbnsd5'
 ADMIN_PASSWORD = "$2b$12$y92zRYAOVwDC0UCXnuG5ZuiJXxiT.drxRFVBu4HoYKmDMB.e.y5kq"
 
-mode = 0
+mode = 1
 
 if mode == 0:
     MANAGER_SERVER_API = "http://localhost:8000/api"
