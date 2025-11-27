dbc1964fbdad154a5d0cf95fb9966c9eb97b55ef
yojun313 <moonyojun@gmail.com>
2025-11-28 03:09:48 +0900

    feat: implement SSH tunneling for MongoDB connection and enhance web crawler functionality

--- crawler/Package/ToolModule.py
+++ crawler/Package/ToolModule.py
@@ -18,6 +18,19 @@ load_dotenv()
 DB_IP = '121.152.225.232'
 LOCAL_IP = '192.168.0.3'
 
+SSH_HOST = os.getenv("SSH_HOST")
+SSH_PORT = int(os.getenv("SSH_PORT", 22))
+SSH_USER = os.getenv("SSH_USER")
+SSH_KEY = os.getenv("SSH_KEY")
+
+# MongoDB 설정
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_AUTH_DB = os.getenv("MONGO_AUTH_DB", "admin")
+
+
 class ToolModule:
     def __init__(self):
         pass
@@ -41,8 +54,32 @@ class ToolModule:
         return returnData
     
     def mongoDB(self):
-        uri = os.getenv("MONGO_URI")
-        self.mongoClient = MongoClient(uri)
+        hostname = socket.gethostname()
+        is_server = ("knpu" in hostname or "server" in hostname)  # 서버 이름 기준으로 판단
+
+        if is_server:
+            # 서버 내부에서 실행 → 로컬 MongoDB 바로 사용
+            self.mongoClient = MongoClient(
+                f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}"
+                f"@localhost:{MONGO_PORT}/?authSource={MONGO_AUTH_DB}"
+            )
+        else:
+            import warnings
+            warnings.filterwarnings("ignore", module="paramiko")
+            from sshtunnel import SSHTunnelForwarder
+            # 외부에서 실행 → SSH 터널 사용
+            server = SSHTunnelForwarder(
+                (SSH_HOST, SSH_PORT),
+                ssh_username=SSH_USER,
+                ssh_pkey=SSH_KEY,
+                remote_bind_address=(MONGO_HOST, MONGO_PORT)
+            )
+            server.start()
+
+            self.mongoClient = MongoClient(
+                f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}"
+                f"@127.0.0.1:{server.local_bind_port}/?authSource={MONGO_AUTH_DB}"
+            )
     
     def read_txt(self, filepath):
         txt_path = filepath
