e21614c377b43386591a6c5316658dd6cefdec1e
yojun313 <moonyojun@gmail.com>
2025-11-28 13:12:54 +0900

    refactor: enhance password input dialog and improve validation feedback

--- manager/app/services/auth.py
+++ manager/app/services/auth.py
@@ -20,19 +20,17 @@ def checkPassword(parent, admin=False, string=""):
                 input_dialog.setWindowTitle(string)
         else:
             input_dialog.setWindowTitle('Admin Mode')
-        input_dialog.setLabelText('Enter password:')
-        input_dialog.setTextEchoMode(QLineEdit.Password)
-        input_dialog.resize(300, 200)  # 원하는 크기로 설정
 
-        # 비밀번호 입력 창 띄우기
+        input_dialog.setLabelText('Enter password:')
+        input_dialog.setTextEchoMode(QLineEdit.EchoMode.Password)
+        input_dialog.resize(300, 200)
+
         ok = input_dialog.exec()
         password = input_dialog.textValue()
 
-        # 영어 알파벳만 있는지 확인
         if re.match("^[a-zA-Z0-9!@#$%^&*()_+=-]*$", password):
             return ok, password
         else:
-            # 오류 메시지 표시
             QMessageBox.warning(parent, "Invalid Input", "영어로만 입력 가능합니다")
 
 def loginProgram(parent):
