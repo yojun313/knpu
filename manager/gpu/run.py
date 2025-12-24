726311f846c17bae634c501276328ad5a4ee68a9
yojun313 <moonyojun@gmail.com>
2025-12-24 18:58:36 +0900

    fix: update bind address to 0.0.0.0:8001 in run.py

--- manager/gpu/run.py
+++ manager/gpu/run.py
@@ -1,6 +1,6 @@
 # gunicorn app.main:app -c run.py
 # uvicorn app.main:app --host 0.0.0.0 --port 8000
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:8001"
 workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 0
