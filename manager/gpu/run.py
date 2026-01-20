059434e8382327b6f426263e8f6cf2bf93807e0f
yojun313 <moonyojun@gmail.com>
2026-01-20 23:08:05 +0900

    fix: update bind address to use port 8002 in run configuration

--- manager/gpu/run.py
+++ manager/gpu/run.py
@@ -1,6 +1,6 @@
 # gunicorn app.main:app -c run.py
 # uvicorn app.main:app --host 0.0.0.0 --port 8000
-bind = "0.0.0.0:8001"
+bind = "0.0.0.0:8002"
 workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 0
