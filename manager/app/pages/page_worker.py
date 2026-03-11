3b910f7b74e92320ceb5e8b397cb9df494ec5c62
yojun313 <moonyojun@gmail.com>
2026-03-11 23:26:18 +0900

    fix: remove critical error message box in worker_failed method

--- manager/app/pages/page_worker.py
+++ manager/app/pages/page_worker.py
@@ -14,7 +14,6 @@ class Manager_Worker:
             QMessageBox.warning(self.main, "실패", f"작업을 실패했습니다.\n{message}")
     
     def worker_failed(self, error_message: str):
-        QMessageBox.critical(self.main, "오류 발생", f"오류가 발생했습니다:\n{error_message}")
         programBugLog(self.main, error_message)
     
     def connectWorkerForDownloadDialog(self, worker, downloadDialog, thread_name):
